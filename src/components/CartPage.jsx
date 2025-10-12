4243cc7f09bddab09dbeb4e6ac32139e27885c8a
chavimiller <chaviweisman@gmail.com>
2025-10-12 13:45:08 +0300

    Add loading text while api is fetching

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,11 +1,21 @@
 import { useCart } from "../hooks/CartContext";
+import Card from "./Card";
 
 const CartPage = () => {
   const { cart, setCart } = useCart();
 
   return (
     <>
-      <div className="cart-container"></div>
+      <div className="cart-container">
+        {cart.forEach((product) => (
+          <Card
+            id={product.id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
     </>
   );
 };
